026157b95b267d42dc7e6e6ab3073cfb787d33c8
LuisLunaV <luis.a.luna.v@outlook.com>
2021-11-08 01:03:51 -0600

    Se actualiza componentes.js, se agrega un './' en el <img>

--- docs/js/componentes.js
+++ docs/js/componentes.js
@@ -65,7 +65,7 @@ export const crearHtml=( tipo1, tipo2 ) => {
 
         limpiarHtml();
 
-        },2000); 
+        },12000); 
         
     }else{
 
@@ -112,7 +112,7 @@ reiniciar();
                     reiniciar();
             }    
 
-            },2000)
+            },12000)
             
             
         }
