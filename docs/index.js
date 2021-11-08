c9fdb208306f43e4eb77fe691604c2869144fd0a
LuisLunaV <luis.a.luna.v@outlook.com>
2021-11-07 23:16:17 -0600

    Juego realizado con las tecnologias basicas de FrontEnd, HTML, CSS y JS

---
+++ docs/index.js
@@ -0,0 +1,25 @@
+import {manos,crearHtml,computadora, lapsoBtn, piedra, papel, tijera, turno} from './js/index.js';
+
+
+piedra.addEventListener('click',()=>{
+
+    crearHtml('piedra', computadora(manos));
+    lapsoBtn();
+   
+});
+
+papel.addEventListener('click',()=>{
+
+    crearHtml('papel', computadora(manos));
+    lapsoBtn();
+
+});
+
+tijera.addEventListener('click',()=>{
+
+    crearHtml('tijera', computadora(manos)); 
+    lapsoBtn();
+
+});
+
+turno();
---
+++ docs/js/index.js
@@ -0,0 +1,16 @@
+// import { crearHtml,manos,computadora,limpiarHtml,lapsoBtn } from './js/componentes.js';
+import { crearHtml, manos, computadora, lapsoBtn,limpiarHtml, piedra, papel, tijera } from './componentes.js';
+import { turno, turnos, puntos, puntosPc, reiniciar } from './puntajes.js';
+export{
+    crearHtml,
+    manos,
+    computadora,
+    lapsoBtn,
+    limpiarHtml,
+    piedra,
+    papel,
+    tijera,
+    turno,
+    turnos,
+    reiniciar
+}
